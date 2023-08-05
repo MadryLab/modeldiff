bc414d4e917a913d4217d2ad577197994b3a39e6
Harshay <harshay.rshah@gmail.com>
2023-08-05 08:01:05 -0400

    add script to download living17 checkpoints

---
+++ notebooks/download_living17_checkpoints.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Downloads resnet18 checkpoints trained on the Living17 dataset with and without standard data augmentation
+
+script_dir=$(cd -P -- "$(dirname "$0")" && pwd -P)
+ckpt_dir=$script_dir/checkpoints
+mkdir $ckpt_dir
+wget -O $ckpt_dir/living17.pt https://www.dropbox.com/scl/fi/2ezqphmgwetw2j477i6r6/living17_ckpts.pt?rlkey=5ep3p1hhw8qfaxdlqjzd9m2q3&dl=0 
--- notebooks/download_scores.sh
+++ notebooks/download_scores.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+# Downloads pre-computed datamodel and TRAK scores for all learning algorithms / case studies in the paper
 wget -O datamodels.zip https://www.dropbox.com/s/9ohxrrba8wb2piv/datamodels.zip?dl=0
-unzip datamodels.zip 
+unzip datamodels.zip
 rm -rf __MACOSX
-rm -rf datamodels.zip 
+rm -rf datamodels.zip
